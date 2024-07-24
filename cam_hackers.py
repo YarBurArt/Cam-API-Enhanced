43488553c8af38ccb315c68f41259ca35d857130
yarburart <yaroslav.chayk@gmail.com>
2024-07-24 22:43:30 +0500

    rewrite to pylint standart

--- cam_hackers.py
+++ cam_hackers.py
@@ -1,26 +1,36 @@
-
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
-#github.com/AngelSecurityTeam/Cam-Hackers
+# github.com/AngelSecurityTeam/Cam-Hackers
+import sys
+import re
+import urllib.request
+import json
+import requests
 
-import requests, re , colorama ,random
-from requests.structures import CaseInsensitiveDict
-colorama.init()
+def get_data(url, headers):
+    req = urllib.request.Request(url, headers=headers)
+    with urllib.request.urlopen(req) as resp:
+        return resp.read().decode('utf-8')
 
-url = "http://www.insecam.org/en/jsoncountries/"
+def save_ips_to_file(country, ips):
+    with open(f'{country}.txt', 'w', encoding="utf-8") as f:
+        for ip in ips:
+            print("\n\033[1;31m", ip)
+            f.write(f'{ip}\n')
 
-headers = CaseInsensitiveDict()
-headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"
-headers["Cache-Control"] = "max-age=0"
-headers["Connection"] = "keep-alive"
-headers["Host"] = "www.insecam.org"
-headers["Upgrade-Insecure-Requests"] = "1"
-headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+BASE_URL = "http://www.insecam.org/en/jsoncountries/"
 
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Cache-Control": "max-age=0",
+    "Connection": "keep-alive",
+    "Host": "www.insecam.org",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+}
 
-resp = requests.get(url, headers=headers)
-
-data = resp.json()
+rsp = get_data(BASE_URL, headers)
+data = json.loads(rsp)
 countries = data['countries']
 
 print("""
@@ -33,37 +43,28 @@ print("""
 \033[1;31m                                                                        ANGELSECURITYTEAM \033[1;31m\033[1;37m""")
 
 for key, value in countries.items():
-    print(f'Code : ({key}) - {value["country"]} / ({value["count"]})  ')
-    print("")
+    print(f'Code : ({key}) - {value["country"]} / ({value["count"]})  \n')
 
 
 
 try:
-   
-
     country = input("Code(##) : ")
-    res = requests.get(
-        f"http://www.insecam.org/en/bycountry/{country}", headers=headers
-    )
+    res = get_data(f"http://www.insecam.org/en/bycountry/{country}", headers=headers)
     last_page = re.findall(r'pagenavigator\("\?page=", (\d+)', res.text)[0]
 
     for page in range(int(last_page)):
         res = requests.get(
             f"http://www.insecam.org/en/bycountry/{country}/?page={page}",
-            headers=headers
-        )
+            headers=headers, timeout=5)
         find_ip = re.findall(r"http://\d+.\d+.\d+.\d+:\d+", res.text)
-    
-        with open(f'{country}.txt', 'w') as f:
-          for ip in find_ip:
-              print("")
-              print("\033[1;31m", ip)
-              f.write(f'{ip}\n')
+
+        save_ips_to_file(country, find_ip)
 except:
-    pass
+    print("Something went wrong and now you have to deal with it, try python -v cam_hackers.py :)")
+    sys.exit(1)
+
 finally:
     print("\033[1;37m")
     print('\033[37mSave File :'+country+'.txt')
 
-    exit()
-
+    sys.exit(0)
